50be0bbe4cf8a0d2b6263005d777bae65af8a5ad
rn-santos227 <rn_santos227@yahoo.com>
2024-02-22 11:08:29 +0800

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,16 @@
+require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+const API_URL = process.env.JSON_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+module.exports = {
+  solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: API_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+};
